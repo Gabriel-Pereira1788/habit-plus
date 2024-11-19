279b41aa6bda633e6e697138613c83e21e15e425
Gabriel-Pereira1788 <gabriel.pereira1788@gmail.com>
2024-11-18 22:01:36 -0300

    Initial Commit

---
+++ HabitPlus/HabitPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitPlusApp.swift
+//  HabitPlus
+//
+//  Created by Gabriel Pereira on 18/11/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
